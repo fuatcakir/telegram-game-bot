06b6438d3cfb33f25de25880d1419ee77fcbfcba
fuatcakir <37143344+fuatcakir@users.noreply.github.com>
2020-05-08 23:53:31 +0300

    try catch

--- index.js
+++ index.js
@@ -2,62 +2,116 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '1124541868:AAHBjkJMrAvCqC22wX-D7KpOGFtAKwj5i6Y';
 
-const bot = new TelegramBot(token, { polling: true });
+try {
 
 
-let leaders = {};
-var fs = require("fs");
-console.log("\n *STARTING* \n");
-var contents = fs.readFileSync("sorban.json");
-var jsonContent = JSON.parse(contents);
-
-let systemOnOff = "Off";
-bot.onText(/\/sualci (.+)/, (msg, match) => {
-
-  console.log('object :>> ', jsonContent[2]['q']);
-  const chatId = msg.chat.id;
-  const resp = match[1];
-
-  if (resp == 'start') {
-    systemOnOff = "On";
-  } else if (resp == 'stop') {
-    systemOnOff = "Off";
-  } else if (resp == 'board') {
-    bot.sendMessage(msg.chat.id, "Puanlar:  " + JSON.stringify(leaders));
-  } else if (resp == 'info') {
-    bot.sendMessage(msg.chat.id, "mailto : fuat.cakir@outlook.com");
-  }
-
-});
+  const bot = new TelegramBot(token, { polling: true });
 
 
-let askedSeqNo = 1;
-let answeredRight = false;
-let question = {};
-let answerLength = 1;
-let hintCount = 0;
+  let leaders = {};
+  var fs = require("fs");
+  console.log("\n *STARTING* \n");
+  var contents = fs.readFileSync("sorban.json");
+  var jsonContent = JSON.parse(contents);
 
-bot.on('message', (msg) => {
-  console.log('msg income ', msg);
-  if (systemOnOff == "On" && !(msg.text.toString().includes('board') || msg.text.toString().includes('info') )) {
+  let systemOnOff = "Off";
+  bot.onText(/\/sualci (.+)/, (msg, match) => {
 
-
-    console.log('msg income ');
+    console.log('object :>> ', jsonContent[2]['q']);
     const chatId = msg.chat.id;
-    console.log('msg msg.chat.id ' + chatId);
-    if (question['a']) {
-      if (isAnswerRight(question['a'], msg.text.toString())) {
-        console.log('DOGRU');
-        answeredRight = true;
-        bot.sendMessage(msg.chat.id, "Doğru Cevap  " + msg.from.first_name);
-        if (leaders[msg.from.first_name]) {
-          leaders[msg.from.first_name] = leaders[msg.from.first_name] + 1;
+    const resp = match[1];
+
+    if (resp == 'start') {
+      systemOnOff = "On";
+    } else if (resp == 'stop') {
+      systemOnOff = "Off";
+    } else if (resp == 'board') {
+      bot.sendMessage(msg.chat.id, "Puanlar:  " + JSON.stringify(leaders));
+    } else if (resp == 'info') {
+      bot.sendMessage(msg.chat.id, "mailto : fuat.cakir@outlook.com");
+    }
+
+  });
+
+
+  let askedSeqNo = 1;
+  let answeredRight = false;
+  let question = {};
+  let answerLength = 1;
+  let hintCount = 0;
+
+  bot.on('message', (msg) => {
+    console.log('msg income ', msg);
+    if (systemOnOff == "On" && !(msg.text.toString().includes('board') || msg.text.toString().includes('info'))) {
+
+
+      console.log('msg income ');
+      const chatId = msg.chat.id;
+      console.log('msg msg.chat.id ' + chatId);
+      if (question['a']) {
+        if (isAnswerRight(question['a'], msg.text.toString())) {
+          console.log('DOGRU');
+          answeredRight = true;
+          bot.sendMessage(msg.chat.id, "Doğru Cevap  " + msg.from.first_name);
+          if (leaders[msg.from.first_name]) {
+            leaders[msg.from.first_name] = leaders[msg.from.first_name] + 1;
+          } else {
+            leaders[msg.from.first_name] = 1;
+          }
+
+
+          console.log('soru geliyor');
+          while (true) {
+            askedSeqNo = randomIntInc(1, 2345);
+            question = jsonContent[askedSeqNo];
+            if (question) {
+              break;
+            } else {
+              askedSeqNo = randomIntInc(1, 2345);
+              question = jsonContent[askedSeqNo];
+            }
+          }
+          bot.sendMessage(chatId, "Soru : " + question['q'] + " ?");
+          hintCount = 0;
         } else {
-          leaders[msg.from.first_name] = 1;
+          answeredRight = false;
+          console.log('yanlis cevap');
+          // bot.sendMessage(chatId, "Yanlış Cevap  " + msg.from.first_name);
+
+          hintCount++;
+
+          if (hintCount % 2 == 0) {
+            let hintindx = hintCount / 2;
+            if (question['a'].slice(0, hintindx) == question['a']) {
+              answeredRight = true;
+              bot.sendMessage(chatId, "Doğru  Cevap : " + question['a'].toLowerCase());
+
+              console.log('soru geliyor');
+              while (true) {
+                askedSeqNo = randomIntInc(1, 2345);
+                question = jsonContent[askedSeqNo];
+                if (question) {
+                  break;
+                } else {
+                  askedSeqNo = randomIntInc(1, 2345);
+                  question = jsonContent[askedSeqNo];
+                }
+              }
+
+              bot.sendMessage(chatId, "Soru : " + question['q'] + " ?");
+              hintCount = 0;
+
+            } else {
+              console.log('ipucu veriliyor ');
+              answeredRight = false;
+              answerLength = question['a'].length + 1;
+              bot.sendMessage(chatId, "ipucu : " + question['a'].substr(0, hintindx).padEnd(question['a'].length, "*"));
+            }
+          }
+
         }
-
-
-        console.log('soru geliyor');
+      } else {
+        console.log('yeni soru veriliyor ');
         while (true) {
           askedSeqNo = randomIntInc(1, 2345);
           question = jsonContent[askedSeqNo];
@@ -68,61 +122,16 @@ bot.on('message', (msg) => {
             question = jsonContent[askedSeqNo];
           }
         }
-        bot.sendMessage(chatId, "Soru : " + question['q'] + " ?");
-        hintCount = 0;
-      } else {
-        answeredRight = false;
-        console.log('yanlis cevap');
-        // bot.sendMessage(chatId, "Yanlış Cevap  " + msg.from.first_name);
-
-        hintCount++;
-
-        if (hintCount % 2 == 0) {
-          let hintindx = hintCount / 2;
-          if (question['a'].slice(0, hintindx) == question['a']) {
-            answeredRight = true;
-            bot.sendMessage(chatId, "Doğru  Cevap : " + question['a'].toLowerCase());
-
-            console.log('soru geliyor');
-            while (true) {
-              askedSeqNo = randomIntInc(1, 2345);
-              question = jsonContent[askedSeqNo];
-              if (question) {
-                break;
-              } else {
-                askedSeqNo = randomIntInc(1, 2345);
-                question = jsonContent[askedSeqNo];
-              }
-            }
-
-            bot.sendMessage(chatId, "Soru : " + question['q'] + " ?");
-            hintCount = 0;
-
-          } else {
-            console.log('ipucu veriliyor ');
-            answeredRight = false;
-            answerLength = question['a'].length + 1;
-            bot.sendMessage(chatId, "ipucu : " + question['a'].substr(0, hintindx).padEnd(question['a'].length, "*"));
-          }
-        }
-
+        bot.sendMessage(chatId, "Soru : " + question['q']);
       }
-    } else {
-      console.log('yeni soru veriliyor ');
-      while (true) {
-        askedSeqNo = randomIntInc(1, 2345);
-        question = jsonContent[askedSeqNo];
-        if (question) {
-          break;
-        } else {
-          askedSeqNo = randomIntInc(1, 2345);
-          question = jsonContent[askedSeqNo];
-        }
-      }
-      bot.sendMessage(chatId, "Soru : " + question['q']);
     }
-  }
-});
+  });
+
+
+} catch (error) {
+  console.log('hata var !!!');
+  console.error(error);
+}
 
 function randomIntInc(low, high) {
   return Math.floor(Math.random() * (high - low + 1) + low)
